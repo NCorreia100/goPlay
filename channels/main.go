608c7d28a4e31c3b19d5181e1b9eebc4ce7e36f6
nuno <correia.nuno@hearst.com>
2020-11-01 19:07:03 -0500

    using go routines and channels

---
+++ channels/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func checkLink(link string, ch chan string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link + " is down!")
+	} else {
+		fmt.Println(link + " OK!")
+	}
+	ch <- link
+}
+func main() {
+	// msec := time.Now().UnixNano() / 1000000
+	var ch = make(chan string)
+	links := []string{
+		"https://google.com",
+		"https://facebook.com",
+		"https://stackoverflow.com",
+		"https://golang.org",
+		"https://amazon.com",
+	}
+
+	for _, l := range links {
+		go checkLink(l, ch)
+		// fmt.Println(<-ch) ---> if I print the channel here isntead, it will block the execution of the main routine
+	}
+
+	qty := 0
+	for c := range ch {
+		if qty%5 == 0 {
+			time.Sleep(time.Second)
+		}
+		qty++
+
+		//restart go routines
+		go checkLink(c, ch)
+	}
+	// fmt.Println("time passed", (time.Now().UnixNano()/1000000)-msec)
+}
